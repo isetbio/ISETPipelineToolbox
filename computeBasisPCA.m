c3ef565f0732a4231cd14c3b0463ad46b8844af8
lingqiz <zlqzcc@gmail.com>
2019-03-11 18:13:13 -0400

    minor update

--- computeBasisPCA.m
+++ computeBasisPCA.m
@@ -1,33 +1,35 @@
-function [regBasis, mu] = computeBasisPCA(imageSet, imageDim)
+function [regBasis, mu] = computeBasisPCA(imageSet, imageDim, vis)
 %COMPUTEBASISPCA
 
 [pcaBasis, ~, pcaVar, ~, ~, mu] = pca(imageSet);
 
 % Visulization
-basisSize = imageDim * imageDim * 3;
-basisSet = reshape(pcaBasis, [imageDim, imageDim, 3, basisSize]);
-
-allDim   = floor(sqrt(basisSize)); 
-basisImg = zeros(allDim * imageDim, allDim * imageDim, 3);
-
-for i = 1:allDim
-    for j = 1:allDim
-        % Select each Basis Image
-        idx   = (i - 1) * allDim + j;
-        basis = basisSet(:, :, :, idx);
-        basis = basis(:);
-        
-        % Normalization
-        basis = (basis - min(basis))/(max(basis) - min(basis));
-
-        % Add to Display Image 
-        basisImg( (i-1) * imageDim + 1:i * imageDim, (j-1) * imageDim + 1:j * imageDim, :) ...
-            = reshape(basis, imageDim, imageDim, 3);
+if(vis)
+    basisSize = imageDim * imageDim * 3;
+    basisSet = reshape(pcaBasis, [imageDim, imageDim, 3, basisSize]);
+    
+    allDim   = floor(sqrt(basisSize));
+    basisImg = zeros(allDim * imageDim, allDim * imageDim, 3);
+    
+    for i = 1:allDim
+        for j = 1:allDim
+            % Select each Basis Image
+            idx   = (i - 1) * allDim + j;
+            basis = basisSet(:, :, :, idx);
+            basis = basis(:);
+            
+            % Normalization
+            basis = (basis - min(basis))/(max(basis) - min(basis));
+            
+            % Add to Display Image
+            basisImg( (i-1) * imageDim + 1:i * imageDim, (j-1) * imageDim + 1:j * imageDim, :) ...
+                = reshape(basis, imageDim, imageDim, 3);
+        end
     end
+    
+    figure;
+    imshow(basisImg, 'InitialMagnification', 50);
 end
-
-figure;
-imshow(basisImg, 'InitialMagnification', 50);
 
 scaleMatrix = diag(sqrt(pcaVar));
 regBasis    = pcaBasis * scaleMatrix;
