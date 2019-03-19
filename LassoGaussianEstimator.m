3aa3078bf55696e148a26b9c3970f097fa687720
lingqiz <zlqzcc@gmail.com>
2019-03-19 11:28:59 -0400

    update solver option

--- LassoGaussianEstimator.m
+++ LassoGaussianEstimator.m
@@ -4,13 +4,15 @@ classdef LassoGaussianEstimator < BayesianEstimator
         Verbose;
         Lambda;
         IterationLimit;
+        Solver;
     end
         
     methods
         function this = LassoGaussianEstimator(render, basis, mu)
             this@BayesianEstimator(render, basis, mu);
             this.Lambda = 1;
-            this.IterationLimit = 5e3;
+            this.Solver = 'sparsa';
+            this.IterationLimit = 5e3;            
         end
         
         function reconImage = estimate(this, input, varargin)
@@ -21,9 +23,17 @@ classdef LassoGaussianEstimator < BayesianEstimator
             lambda = p.Results.regularization / length(input);
             target = input' - this.combinedBias;
             
-            Mdl = fitrlinear(this.combinedRender, target, 'Lambda', lambda, ...
-                'Learner', 'leastsquares', 'Regularization', 'lasso', 'FitBias', false, 'Solver', 'sparsa', 'Verbose', ...
-                this.Verbose, 'BetaTolerance', 1e-8, 'GradientTolerance', 1e-8, 'IterationLimit', this.IterationLimit);
+            if(strcmp(this.Solver, 'sparsa'))
+                Mdl = fitrlinear(this.combinedRender, target, 'Lambda', lambda, ...
+                    'Learner', 'leastsquares', 'Regularization', 'lasso', 'FitBias', false, 'Solver', 'sparsa', 'Verbose', ...
+                    this.Verbose, 'BetaTolerance', 1e-8, 'GradientTolerance', 1e-8, 'IterationLimit', this.IterationLimit);
+            elseif(strcmp(this.Solver, 'sgd'))
+                Mdl = fitrlinear(this.combinedRender, target, 'Lambda', lambda, ...
+                    'Learner', 'leastsquares', 'Regularization', 'lasso', 'FitBias', false, 'Solver', 'sgd', 'Verbose', ...
+                    this.Verbose, 'BetaTolerance', 1e-8, 'GradientTolerance', 1e-8, 'IterationLimit', this.IterationLimit, 'BatchSize', length(input));
+            else
+                error('Invalid Solver Name.')
+            end
             reconImage = (this.Basis(:, 1:this.nDim) * Mdl.Beta + this.Mu)';
         end
         
@@ -41,6 +51,10 @@ classdef LassoGaussianEstimator < BayesianEstimator
         
         function setIterationLimit(this, limit)
             this.IterationLimit = limit;
+        end
+        
+        function setSolver(this, solver)
+            this.Solver = solver;
         end
     end
 end
