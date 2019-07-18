f61b463e3902b2cf30a588f6dcee92b245d8488a
lingqiz <zlqzcc@gmail.com>
2019-07-18 10:30:23 -0400

    restruct

---
+++ PatchEstimator.m
@@ -0,0 +1,94 @@
+classdef PatchEstimator < handle
+    
+    properties
+        Render;  % Render matrix estimated from isetbio rountine
+        Basis;   % Image Basis (i.e., PCA or ICA basis)
+        Mu;      % Vector of the mean value of each pixel
+        Disp;    % Display option for optimization
+        Lambda;  % Regularization
+        Size;    % Size of original image
+        Patch;   % Size of reconstruction blocks
+        Stride;  % Step stride size
+    end
+    
+    methods (Abstract)
+        
+        % Prior on one small patch of the image
+        [nlogll, gradient] = priorPatch(this, patchVec)
+
+    end
+
+    methods
+        
+        % Constructor for the estimator
+        function this = PatchEstimator(render, basis, mu, lambda, stride, imageSize)
+            this.Render = render;
+            this.Basis  = basis;
+            this.Mu     = mu;
+            this.Lambda = lambda;
+            this.Stride = stride;
+            this.Size   = imageSize;
+            this.Patch  = sqrt(size(basis, 1) / 3); % Assume square basis image
+            this.Disp   = 'iter';
+        end
+        
+        % Gaussian approximation of the likelihood
+        function [nlogll, gradient] = likelihood(this, measure, imageVec)
+            pred   = this.Render * imageVec;
+            nlogll = sum((pred - measure) .^ 2);
+            gradient = (2 * (pred - measure)' * this.Render)';
+        end
+                        
+        % Prior loss and gradient
+        function [nlogll, gradient] = prior(this, image)
+            nlogll   = 0;
+            gradient = zeros(length(image(:)), 1);
+            
+            for x = 1:this.Stride:(this.Size(1) - this.Patch + 1)
+                for y = 1:this.Stride:(this.Size(2) - this.Patch + 1)
+                    idxX = x:1:(x+this.Patch-1);
+                    idxY = y:1:(y+this.Patch-1);
+                    imagePatch = image(idxX, idxY, :);
+                    [nlogllPatch, gradientPatch] = this.priorPatch(imagePatch(:));
+                    
+                    nlogll = nlogll + nlogllPatch;
+                    
+                    gradImage = zeros(this.Size);
+                    gradImage(idxX, idxY, :) = reshape(gradientPatch, [this.Patch, this.Patch, 3]);
+                    
+                    gradient = gradient + gradImage(:);
+                end
+            end
+            
+            nlogll = this.Lambda * nlogll;
+            gradient = this.Lambda * gradient;
+        end
+        
+        function [loss, gradient] = reconObjective(this, measure, imageVec)
+            [nlogllPrior, gradientPrior] = this.prior(reshape(imageVec, this.Size));
+            [nlogllLlhd,  gradientLlhd]  = this.likelihood(measure, imageVec);
+            
+            loss = nlogllPrior + nlogllLlhd;
+            gradient = gradientPrior + gradientLlhd;
+        end
+        
+        function reconstruction = estimate(this, measure, maxIter, init)
+            loss = @(x) this.reconObjective(measure, x);
+
+            if ~exist('maxIter', 'var')
+                maxIter = 1e3;
+            end
+
+            if ~exist('init', 'var')
+                init = rand([prod(this.Size), 1]);
+            end
+            
+            options  = optimset('GradObj','on', 'Display', 'iter', 'MaxIter', maxIter);
+            solution = fminlbfgs(loss, init, options);
+            
+            reconstruction = reshape(solution, this.Size);
+        end
+        
+    end
+end
+
--- SparsePatchEstimator.m
+++ SparsePatchEstimator.m
@@ -1,20 +1,21 @@
-classdef GaussianPatchEstimator < PatchEstimator
-    
+classdef SparsePatchEstimator < PatchEstimator
+
     methods
-        
+            
         % Constructor for the estimator
-        function this = GaussianPatchEstimator(render, basis, mu, lambda, stride, imageSize)
+        function this = SparsePatchEstimator(render, basis, mu, lambda, stride, imageSize)
             this@PatchEstimator(render, basis, mu, lambda, stride, imageSize);
         end
         
         % Patchwise Gaussian prior and gradient
         function [nlogll, gradient] = priorPatch(this, patchVec)
             projection = this.Basis * (patchVec - this.Mu);
-            nlogll   = 0.5 * sum(projection .^ 2);
-            gradient = projection' * this.Basis;
-        end
-               
-    end
-    
-end
+            nlogll   = sum(abs(projection));
 
+            zero = 1e-2;
+            gradient = ((projection > zero) - (projection < -zero))' * this.Basis;             
+        end
+            
+    end
+
+end
