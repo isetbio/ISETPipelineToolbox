f42ee2f23e47b326a0a95d169d7dffda6355c9bb
lingqiz <zlqzcc@gmail.com>
2019-11-29 00:02:17 -0500

    single point render matrix

--- ConeResponse.m
+++ ConeResponse.m
@@ -332,14 +332,14 @@ classdef ConeResponse < handle
             testInput = rand(imageSize);
             [~, ~, testLinear, testCone] = this.compute(testInput);
             
-            renderMtx = zeros(length(testCone), length(testLinear(:)));
+            renderMtx = zeros(length(testCone), length(testLinear(:)), 'single');
             
             parfor idx = 1:length(testLinear(:))
                 input = zeros(size(testLinear));
                 input(idx) = 1.0;
                 
                 [~, ~, ~, coneVec] = this.compute(input);
-                renderMtx(:, idx) = coneVec;
+                renderMtx(:, idx) = single(coneVec);
             end
             
             if validation
