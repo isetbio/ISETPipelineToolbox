78063b2928cc4ded1f958c4ea178a168fb1f415b
Carlos <carlos@modv-ve703-01252.apn.wlan.private.upenn.edu>
2022-09-15 11:14:08 -0400

    Add option for domain assignments in visualizeMosaic

--- coneMosaic/ConeResponseCmosaic.m
+++ coneMosaic/ConeResponseCmosaic.m
@@ -36,7 +36,8 @@ classdef ConeResponseCmosaic < ConeResponse
         end
         
         % Override the visualization with new method
-        function visualizeMosaic(this, figureHandle, axesHandle)
+        function visualizeMosaic(this, figureHandle, axesHandle, ...
+                domainVisualizationLimits)
             if ~exist('figureHandle', 'var')
                 figureHandle = figure();
             end
@@ -45,8 +46,13 @@ classdef ConeResponseCmosaic < ConeResponse
                 axesHandle = [];
             end
 
+            if ~exist('domainVisualizationLimits', 'var')
+                domainVisualizationLimits = [];
+            end
+
             this.Mosaic.visualize('figureHandle', figureHandle, ...
-                                  'axesHandle', axesHandle);
+                                  'axesHandle', axesHandle, ...
+                                  'domainVisualizationLimits', domainVisualizationLimits);
         end
         
         function visualizeExcitation(this, figureHandle, axesHandle)
