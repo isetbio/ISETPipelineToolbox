9025102407f48d9f5483e023ef59df2695f63334
David Brainard <brainard@psych.upenn.edu>
2024-09-07 09:36:09 -0400

    Pulled this out as a separate function

--- coneMosaic/ConeResponseCmosaic.m
+++ coneMosaic/ConeResponseCmosaic.m
@@ -207,27 +207,6 @@ classdef ConeResponseCmosaic < ConeResponse
                 axis square;
             end
         end
-        
-        % Render matrix version where each rgb channel of monitor
-        % representation has been scaled by the corresponding entry
-        % of scaleVector. Used to tweak monitor scaling without needing
-        % to recompute all of the nice render matrices we may already have.
-        function renderMtx = scaleRenderMtx(this, renderMtx, scaleVector, varargin)
-
-            [m,n] = size(renderMtx);
-            nPrimaries = length(scaleVector);
-            nPixels = n/nPrimaries;
-            if (nPixels ~= floor(nPixels))
-                error('Logic error recreating number of pixels from render matrix');
-            end
-
-            for jj = 1:nPixels
-                for pp = 1:nPrimaries
-                   renderMtx(:,jj+(pp-1)*nPixels) = renderMtx(:,jj+(pp-1)*nPixels)*scaleFactor(pp)
-                end
-            end
-
-        end
     end
 end
 
---
+++ imageEstimator/ScaleRenderMtx.m
@@ -0,0 +1,20 @@
+function renderMtx = ScaleRenderMtx(renderMtx, scaleVector, varargin)
+% Render matrix version where each rgb channel of monitor
+% representation has been scaled by the corresponding entry
+% of scaleVector. Used to tweak monitor scaling without needing
+% to recompute all of the nice render matrices we may already have.
+
+[~,nCols] = size(renderMtx);
+nPrimaries = length(scaleVector);
+nPixels = nCols/nPrimaries;
+if (nPixels ~= floor(nPixels))
+    error('Logic error recreating number of pixels from render matrix');
+end
+
+for jj = 1:nPixels
+    for pp = 1:nPrimaries
+        renderMtx(:,jj+(pp-1)*nPixels) = renderMtx(:,jj+(pp-1)*nPixels)*scaleFactor(pp);
+    end
+end
+
+end
